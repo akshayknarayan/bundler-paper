5b7134d1dadc7fd48bc65a9c27bcfd6de52a4ee4
Frank Cangialosi <frankc@csail.mit.edu>
2021-04-26 11:30:54 -0400

    fix a couple things for arxiv update

--- arxiv-pkg.sh
+++ arxiv-pkg.sh
@@ -15,7 +15,14 @@ SRC_FILES+=(
 # Grab only the graphics referenced in some TeX file. 
 SRC_PDFS=(`egrep -h "includegraphics" *.tex | grep -v "%" | sed 's/.*{//' | sed 's/}/.pdf/' | sed 's/pdf.pdf/pdf/g' | tr "\n" " "`)
 
+# Manually add others
+OTHER_FILES=(
+    img/noun_Corn_2550101.png
+    img/noun_Beaver_1604196.png
+)
+
 echo "> Found ${#SRC_FILES[@]} tex files: ${SRC_FILES[@]}" 
 echo "> Found ${#SRC_PDFS[@]} graphics: ${SRC_PDFS[@]}"
+echo "> Found ${#OTHER_FILES[@]} other files: ${OTHER_FILES[@]}"
 
-tar -czvf ${MAIN}-arxiv.tgz ${SRC_FILES[@]} ${SRC_PDFS[@]}
+tar -czvf ${MAIN}-arxiv.tgz ${SRC_FILES[@]} ${SRC_PDFS[@]} ${OTHER_FILES[@]}
